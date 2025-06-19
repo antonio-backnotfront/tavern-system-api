b3bd93272937dc69d78c3b8d0a70c1f3d62ffec6
Anton Solianyk <solyanicks@gmail.com>
2025-06-19 21:14:05 +0200

    refactor(CreateAdventurerDto): fix required annotation and keyword for consistency in the code

--- src/TavernSystem.Models/DTO/CreateAdventurerDto.cs
+++ src/TavernSystem.Models/DTO/CreateAdventurerDto.cs
@@ -5,14 +5,18 @@ namespace TavernSystem_Training_1.Models.DTO;
 public class CreateAdventurerDto
 {
     public int? Id { get; set; }
-    [Required]
-    public string Nickname { get; set; }
-    [Required]
-    public int RaceId { get; set; }
-    [Required]
-    public int ExperienceLevelId { get; set; }
-    [Required]
-    [RegularExpression("[A-Z]{2}(000[1-9]{1}|00[1-9]{1}[0-9]{1}|0[1-9]{1}[0-9]{2}|[1-9]{1}[0-9]{3})(0[1-9]{1}|1[0-2]{1})(0[1-9]{1}|1[0-9]{1}|2[0-8]{1})[0-9]{4}[A-Z]{2}")]
-    public string PersonDataId { get; set; }
-}
 
+    [Required] 
+    public required string Nickname { get; set; }
+
+    [Required]
+    public required int RaceId { get; set; }
+
+    [Required] 
+    public required int ExperienceLevelId { get; set; }
+
+    [Required]
+    [RegularExpression(
+        "[A-Z]{2}(000[1-9]{1}|00[1-9]{1}[0-9]{1}|0[1-9]{1}[0-9]{2}|[1-9]{1}[0-9]{3})(0[1-9]{1}|1[0-2]{1})(0[1-9]{1}|1[0-9]{1}|2[0-8]{1})[0-9]{4}[A-Z]{2}")]
+    public required string PersonDataId { get; set; }
+}
